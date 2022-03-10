88c60bdc894e3c6cf9f6ba215929281dfbe34f5e
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2022-03-10 11:13:40 -0300

    10/03/2022

--- src/components/layoult/cima.js
+++ src/components/layoult/cima.js
@@ -6,17 +6,16 @@ function cima(){
     return(
       <div className={styles.cima}>
         <ul>
-           <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li></nav>           
-        </ul>
-        <ul>
-          <li className={styles.cimaLi}><p>Cuacoxo</p></li>
-          <li className={styles.cimaLi}><p>Cousgal</p></li>
-          <li className={styles.cimaLi}><p>Wigen</p></li>
-          <li className={styles.cimaLi}><p>Tardoin</p></li>
-          <li className={styles.cimaLi}><p>Zigawuy</p></li>
-          <li className={styles.cimaLi}><p>Yolxis</p></li>
-          <li className={styles.cimaLi}><p>Zid</p></li>
-          <li className={styles.cimaLi}><p>Drogir</p></li>
+          <div className={styles.cimaC}>
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Cuacoxo</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Cousgal</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Wigen</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Tardoin</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Zigawuy</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Yolxis</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Zid</p></nav>      
+          <nav className={styles.navC}><li><Link to="/Shorts"><img className={styles.icons} src={ico} /></Link></li><p className={styles.cimaLi}>Drogir</p></nav>      
+       </div>
         </ul>
       </div>
     )
